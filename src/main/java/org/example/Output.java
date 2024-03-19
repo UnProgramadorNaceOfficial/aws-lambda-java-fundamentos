2170c46f87ac0296a92e2073991dc6addeeefe99
Santyusbsd <unprogramadornace@gmail.com>
2024-03-18 22:15:58 -0500

    first Commit

---
+++ src/main/java/org/example/Output.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Output {
+    private String response;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
